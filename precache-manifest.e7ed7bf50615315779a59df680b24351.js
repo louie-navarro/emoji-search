8c7386823a94d5949fc268b800c14022825ae5b8
Louie Navarro <louie.navarro@asurion.com>
2023-08-31 12:48:20 +0800

    Updates

---
+++ precache-manifest.e7ed7bf50615315779a59df680b24351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c651673ebb691f6c5f030c2bf502611",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "c765162a3fcefb550ab2",
+    "url": "/emoji-search/static/css/2.5510c45e.chunk.css"
+  },
+  {
+    "revision": "d6570268b415ace1115a",
+    "url": "/emoji-search/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "c765162a3fcefb550ab2",
+    "url": "/emoji-search/static/js/2.120a0f45.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/emoji-search/static/js/2.120a0f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6570268b415ace1115a",
+    "url": "/emoji-search/static/js/main.9d046832.chunk.js"
+  },
+  {
+    "revision": "53f65d824d8bd704bdf5",
+    "url": "/emoji-search/static/js/runtime-main.fcefcbdb.js"
+  }
+]);
